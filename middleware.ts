5b5882f6496d893aadf940f7c859cb4081230ea2
Devin Harris <hello@devinhrrs.me>
2025-01-11 21:39:44 -0500

    middleware fix for subdomains

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,26 @@ import { NextRequest } from "next/server";
 
 export default function middleware(request: NextRequest) {
 
+    // to load images, stylesheets, javascript and other assests for subdomains
+
     if (request.nextUrl.pathname.startsWith("/_next") || request.nextUrl.pathname.startsWith("/api") || request.nextUrl.pathname.startsWith("/images")) {
         return NextResponse.next();
     }
 
-    const { host } = request.nextUrl;
+    const url = request.nextUrl.clone();
+    const host = request.headers.get("host") || "";
     
-    if (host.startsWith('/store'))
+    const subdomain = host.split(".")[0];
+
+    if (subdomain === "store") {
+        url.pathname = `/store${url.pathname}`;
+        return NextResponse.rewrite(url);
+    }
+
+    if (subdomain && subdomain !== "www") {
+        request.headers.set("x-user-subdomain", subdomain);
+        return NextResponse.redirect(new URL(`/users/${subdomain}`, request.url))
+    }
 
 
     return NextResponse.next();
